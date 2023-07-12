b7025a2042c903a904c727da7eb230ca5f13b19a
francojalil <jalilfranco9@gmail.com>
2023-07-11 23:00:56 -0300

    menu v1

---
+++ menu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+while true; do
+    echo "Ingrese una opción:
+            1 -> GENERAR
+            2 -> DESCOMPRIMIR
+            3 -> PROCESAR
+            4 -> COMPRIMIR
+            * -> SALIR"
+    read OPCION
+
+    if [ "$OPCION" == "*" ]; then
+        break
+    fi
+
+    case $OPCION in
+        1)
+            echo "Ingrese la cantidad de imágenes a generar"
+            read CANTIDAD
+            bash generar.sh $CANTIDAD
+            ;;
+        2)
+            bash descomprimir.sh imagenes.tar.gz suma_verificacion.txt
+            ;;
+        3)
+            bash procesar.sh
+            ;;
+        4)
+            bash comprimir.sh
+            ;;
+        *)
+            echo "Ingrese una opción válida"
+            ;;
+    esac
+done
+
+echo "¡Hasta luego!"
